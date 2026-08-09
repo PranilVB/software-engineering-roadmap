cf742aedc485772b60f71c9fb08e7231b36c5c98
pranilvb <pranilvb@gmail.com>
2026-08-09 14:11:04 +0530

    Packages 1&2

---
+++ Java/OOP/PackageExample/Human.java
@@ -0,0 +1,14 @@
+
+public class Human {
+    int age;
+    String name;
+    int salary;
+    boolean married;
+
+    Human(int age, String name, int salary, boolean married) {
+        this.age = age;
+        this.name = name;
+        this.salary = salary;
+        this.married = married;
+    }
+}
---
+++ Java/OOP/PackageExample/Message2.java
@@ -0,0 +1,7 @@
+package PackageExample;
+
+public class Message2 {
+    public static void main(String[] args) {
+        System.out.println("This is done using package");
+    }
+}
