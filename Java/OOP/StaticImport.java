be986a7a0cc2812b6b37d8310514193da5565443
pranilvb <pranilvb@gmail.com>
2026-08-11 20:37:37 +0530

    Static Import

---
+++ Java/OOP/StaticImport.java
@@ -0,0 +1,7 @@
+public class StaticImport {
+    public static void main(String[] args) {
+        System.out.println("Static Import Demonstrartion");
+        System.out.println(Math.PI);
+        System.out.println(Math.sqrt(625));
+    }
+}
