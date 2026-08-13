884d55feba622d72cb5163733830810427addc79
pranilvb <pranilvb@gmail.com>
2026-08-13 22:57:10 +0530

    Complete OOP 6 concepts and examples

--- Java/OOP/Clones/Human.java
+++ Java/OOP/Clones/Human.java
@@ -3,12 +3,23 @@ package Clones;
 public class Human implements Cloneable{
     int age;
     String name;
+    int[] arr;
     public Human(int age,String name){
         this.age=age;
         this.name=name;
+        this.arr=new int[]{1,2,3,5,6,7,75,3,1,1312};
     }
 
+        // public Object clone()throws CloneNotSupportedException{
+        //     return super.clone();
+
+        // }
         public Object clone()throws CloneNotSupportedException{
-            return super.clone();
+            Human twin= (Human)super.clone();
+            twin.arr=new int[twin.arr.length];
+            for (int i = 0; i < twin.arr.length; i++) {
+                twin.arr[i]=this.arr[i];
+            }
+return twin;
         }
 }
--- Java/OOP/Clones/Main.java
+++ Java/OOP/Clones/Main.java
@@ -1,9 +1,14 @@
 package Clones;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args)throws CloneNotSupportedException {
         Human pranil=new Human(22, "Pranil");
         Human twin=(Human)pranil.clone();
         System.out.println(twin.age);
+        pranil.arr[0]=99;
+        System.out.println(Arrays.toString(pranil.arr));
+        System.out.println(Arrays.toString(twin.arr));
     }
 }
