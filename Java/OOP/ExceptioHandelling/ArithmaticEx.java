af2b8e64230d1a790dedd5f874bd09509a938463
pranilvb <pranilvb@gmail.com>
2026-08-13 21:52:01 +0530

    Arithmatic Exceptions 2

--- Java/OOP/ExceptioHandelling/ArithmaticEx.java
+++ Java/OOP/ExceptioHandelling/ArithmaticEx.java
@@ -3,10 +3,16 @@ public static void main(String[] args) {
     int a=0;
     int b=3;
     try{
-        int c=b/a;
+        devide(a, b);
     }catch(ArithmeticException e){System.out.println(e);}
     finally{
         System.out.println("This will always run");
     }
 }    
+static int devide(int a ,int b){
+    if (a==0) {
+        throw new ArithmeticException();
+    }
+    return b/a;
+}
 }
