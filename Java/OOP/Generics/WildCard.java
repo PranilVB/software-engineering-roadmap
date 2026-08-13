881f9dda1a1f76109607244f1901cc209c09e33c
pranilvb <pranilvb@gmail.com>
2026-08-13 16:34:35 +0530

    Restricted Wildcard Generic custom array list

---
+++ Java/OOP/Generics/WildCard.java
@@ -0,0 +1,57 @@
+public class WildCard <T extends Number>{
+
+    
+    private Object[] data;
+    private static int DEFAULT_SIZE=10;
+    private int size=0;
+    public WildCard(){
+        this.data=new Object[DEFAULT_SIZE];
+    }
+    public void add (T num){
+        if(this.IsFull()){
+            resize();
+        }
+        data[size++]=num;
+    }
+    private void resize() {
+        Object[] temp=new Object[data.length*2];
+        for (int i = 0; i < data.length; i++) {
+            temp[i]=data[i];
+        }
+        data=temp;
+    }
+
+    private boolean IsFull() {
+       return size==data.length;
+    }    
+        public T remove(){
+        T removed=(T)(data[--size]);
+        return removed;
+    }
+    public T get(int index){
+        return (T)data[index];
+    }
+    public int size(){
+        return size;
+    }
+    public void set(int index,T set ){
+        data[index]=set;
+        return;
+    }
+    public static void main(String[] args) {
+        WildCard<Integer> l=new WildCard<>();
+
+        //           Not available Restricted 
+//         WildCard<String> names = new WildCard<>();
+// names.add("Pranil");
+// names.add("Koustubh");
+    // System.out.println(names.get(0));
+
+        for (int i = 0; i < 10; i++) {
+            l.add(i);
+        }
+        System.out.println(l.get(2));
+        System.out.println(l.remove());
+        System.out.println(l.size);
+    }
+}
