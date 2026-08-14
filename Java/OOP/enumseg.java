c962dedd141827cce8fad9cf427416ded899e8af
pranilvb <pranilvb@gmail.com>
2026-08-14 17:42:50 +0530

    Enum in Java

--- Java/OOP/enumseg.java
+++ Java/OOP/enumseg.java
@@ -3,9 +3,12 @@ public class enumseg {
         January, February, March, April, May, June, July, August, September, October, November, December
     }
     public static void main(String[] args) {
-        Year year;
+        Year year=Year.February;
+        // Enum is a special Java type used to define a fixed set of named constant values.
         for (Year year2 : Year.values()) {
             System.out.println(year2);
         }
+        System.out.println(year);
+        System.out.println(Year.October);
     }
 }
