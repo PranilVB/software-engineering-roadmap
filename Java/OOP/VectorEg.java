ed1a1d875fc8bf78fad08fb6687f499653b9881a
pranilvb <pranilvb@gmail.com>
2026-08-14 17:34:23 +0530

    Vectors in Java

---
+++ Java/OOP/VectorEg.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.Vector;
+
+public class VectorEg {
+    public static void main(String[] args) {
+        List<Integer>vector=new Vector<>();
+        vector.add(10);
+        vector.add(12);
+        vector.add(13);
+        vector.add(14);
+        vector.add(15);
+        System.out.println(vector);
+    }
+}
