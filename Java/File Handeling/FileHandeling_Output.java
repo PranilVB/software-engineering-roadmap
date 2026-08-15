d634d32440018178c49bf79502f0c1819d8b79f5
pranilvb <pranilvb@gmail.com>
2026-08-15 23:40:57 +0530

    File handeling File writer

--- Java/File Handeling/FileHandeling_Output.java
+++ Java/File Handeling/FileHandeling_Output.java
@@ -1,20 +1,34 @@
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
+
 public class FileHandeling_Output {
     public static void main(String[] args) {
-        OutputStream os=System.out;
-        System.out.println();
-        try(OutputStreamWriter osw=new OutputStreamWriter(System.out)){
-            osw.write("Hello world  ");
-            osw.write(97);/*aski for character a */
-            osw.write(10);/*new Line  */
-            osw.write('A');
-            osw.write('\n');
-            char[] arr ="Hello world".toCharArray();
-            osw.write(arr);
-            // osw.write(🥳);
+        //Output stereme writer
+    //     OutputStream os=System.out;
+    //     System.out.println();
+    //     try(OutputStreamWriter osw=new OutputStreamWriter(System.out)){
+    //         osw.write("Hello world  ");
+    //         osw.write(97);/*aski for character a */
+    //         osw.write(10);/*new Line  */
+    //         osw.write('A');
+    //         osw.write('\n');
+    //         char[] arr ="Hello world".toCharArray();
+    //         osw.write(arr);
+    //         // osw.write(🥳);
+    //    }
+    //    catch(IOException e){
+    //     System.out.println(e.getMessage());
+    //    }
+
+
+
+       //File writer
+               try(FileWriter fw=new FileWriter("Note.txt")){
+            fw.write("Hello world  ");
+
        }
        catch(IOException e){
         System.out.println(e.getMessage());
