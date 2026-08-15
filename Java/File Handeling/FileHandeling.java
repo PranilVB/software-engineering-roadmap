5d3af74a86bdff85e2f712a913febff9b598d0c8
pranilvb <pranilvb@gmail.com>
2026-08-15 21:31:13 +0530

    File handeling

---
+++ Java/File Handeling/FileHandeling.java
@@ -0,0 +1,34 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FileHandeling  {
+    public static void main(String[] args) {
+    // try(InputStreamReader irs=new InputStreamReader(System.in)){
+        
+    //     System.out.println("Enter some letters :");
+    //     int letter=irs.read();
+    //     while (irs.ready()) {
+    //         System.out.println((char)letter);
+    //         letter=irs.read();
+    //     }
+    //     irs.close();
+    //     System.out.println();
+    // }
+    // catch(IOException e){
+    //     System.out.println(e.getMessage());
+    // }    
+    try(FileReader fr=new FileReader("Note.txt")){
+        int letter=fr.read();
+        while (fr.ready()) {
+            System.out.println((char)letter);
+            letter=fr.read();
+        }
+        fr.close();
+        System.out.println();
+    }
+    catch(IOException e){
+        System.out.println(e.getMessage());
+    }    
+    }   
+}
