e4aab18d815cd188e74370c87ff64910c760d7b1
pranilvb <pranilvb@gmail.com>
2026-08-16 20:27:25 +0530

    Doubly Linked listadd element at begining

---
+++ Java/LinkdList/DoublyLinkedList/DLL.java
@@ -0,0 +1,42 @@
+package DoublyLinkedList;
+
+public class DLL {
+    Node head;
+
+    public void InsertFirst(int value) {
+        Node node = new Node(value);
+        node.next = head;
+        node.prev = null;
+        if (head != null) {
+            head.prev = node;
+        }
+        head = node;
+    }
+
+    public void display() {
+        System.out.print("null <-> ");
+
+        Node node = head;
+        while (node != null) {
+            System.out.print(node.value + " <-> ");
+            node = node.next;
+        }
+        System.out.print(" null");
+    }
+
+    private class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
---
+++ Java/LinkdList/DoublyLinkedList/Main.java
@@ -0,0 +1,16 @@
+package DoublyLinkedList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        DLL list=new DLL();
+        list.InsertFirst(51);
+        list.InsertFirst(23);
+        list.InsertFirst(22);
+        list.InsertFirst(11);
+        list.InsertFirst(54);
+        list.InsertFirst(53);
+        list.InsertFirst(52);
+        list.display();
+    }
+}
