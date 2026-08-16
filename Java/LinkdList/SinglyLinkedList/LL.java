cd6fc53b225812c568915e8fa56f6163181c991a
pranilvb <pranilvb@gmail.com>
2026-08-16 18:15:57 +0530

    Delete First and last in custom linked list

--- Java/LinkdList/SinglyLinkedList/LL.java
+++ Java/LinkdList/SinglyLinkedList/LL.java
@@ -15,6 +15,59 @@ public class LL {
         }
         size+=1; 
     }
+    public void InsertAtIndex(int index,int value){
+        if (index==0) {
+            InsertFirst(value);
+            return;
+        }
+        if (index==size) {
+            InsertLast(value);
+            return;
+        }
+        Node temp=head;
+        for (int i = 1; i < index; i++) {
+            temp=temp.next;
+        }
+        Node node =new Node(value,temp.next);
+            temp.next=node;
+            size ++;
+        }
+        public Node get(int index){
+            Node node =head;
+            for (int i = 0; i < index; i++) {
+                node=node.next;
+            }
+            return node;
+        }
+        public void DeleteLast(){
+            if (size<=1) {
+                DeleteFirst();
+                return;
+            }
+            Node secondlast=get(size-2);
+            tail=secondlast;
+            tail.next=null;
+            
+
+        } 
+    public void DeleteFirst(){
+        head=head.next;
+        if (head==null) {
+            tail=null;
+        }
+        size--;
+    }
+    // public void DeleteLast()
+    public void InsertLast(int value){
+        if (tail==null) {
+            InsertFirst(value);
+            return;
+        }
+        Node node=new Node(value);
+        tail.next=node;
+        tail=node;
+        size+=1; 
+    }
     public void display() {
         Node temp=head;
         while(temp!=null){
--- Java/LinkdList/SinglyLinkedList/Main.java
+++ Java/LinkdList/SinglyLinkedList/Main.java
@@ -9,6 +9,20 @@ public class Main {
         list.InsertFirst(54);
         list.InsertFirst(53);
         list.InsertFirst(52);
+        list.InsertLast(23);
+        list.InsertLast(22);
+        list.InsertLast(11);
+        list.InsertLast(54);
+        list.InsertLast(53);
+        list.InsertLast(52);
+        list.InsertAtIndex(3, 100);
+        System.out.println("Before Deleting ");
+        list.display();
+        System.out.println("After Deleting First "); 
+        list.DeleteFirst();
+        list.display();
+        System.out.println("After deleting last element ");
+        list.DeleteLast();
         list.display();
     }
 }
