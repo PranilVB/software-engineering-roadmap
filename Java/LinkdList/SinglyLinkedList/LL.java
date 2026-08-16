800eb8071b7b47dd9810279201dc0ddbb525ad91
pranilvb <pranilvb@gmail.com>
2026-08-16 14:02:23 +0530

    Custom Singly Linkedlist

---
+++ Java/LinkdList/SinglyLinkedList/LL.java
@@ -0,0 +1,38 @@
+package SinglyLinkedList;
+public class LL {
+    private int size;
+    private Node head;
+    private Node tail;
+    public LL(){
+        this.size=0;
+    }
+    public void InsertFirst(int value){
+        Node node=new Node(value);
+        node.next=head;
+        head=node;
+        if (tail==null) {
+            tail=head;
+        }
+        size+=1; 
+    }
+    public void display() {
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.value+"-->");
+            temp=temp.next;
+        }
+        System.out.println("END");
+        
+    }
+    private class Node{
+        private int value;
+        private Node next ;
+            public Node(int value){
+                this.value=value;
+            }
+            public Node(int value,Node next){
+                this.value=value;
+                this.next=next;
+            }
+    }
+}
---
+++ Java/LinkdList/SinglyLinkedList/Main.java
@@ -0,0 +1,14 @@
+package SinglyLinkedList;
+public class Main {
+    public static void main(String[] args) {
+        LL list=new LL();
+        list.InsertFirst(51);
+        list.InsertFirst(23);
+        list.InsertFirst(22);
+        list.InsertFirst(11);
+        list.InsertFirst(54);
+        list.InsertFirst(53);
+        list.InsertFirst(52);
+        list.display();
+    }
+}
