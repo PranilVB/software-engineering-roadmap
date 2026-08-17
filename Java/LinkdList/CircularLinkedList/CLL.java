0f06a4cea2614986225d0a4f245ad81c22602978
pranilvb <pranilvb@gmail.com>
2026-08-17 22:12:53 +0530

    Circular linked list

---
+++ Java/LinkdList/CircularLinkedList/CLL.java
@@ -0,0 +1,33 @@
+package CircularLinkedList;
+
+public class CLL {
+    private Node head;
+    private Node tail;
+    public CLL() {
+        this.head = null;
+        this.tail = null;
+    }
+        public void InsertValue(int value){
+            Node node=new Node(value);
+            if (head==null) {
+                head=node;
+                tail=node;
+                return;
+        }
+        tail.next=node;
+        node.next=head;
+        tail=node;
+    }
+    private class Node{
+        int value;
+        Node next;
+        public Node(int value) {
+            this.value = value;
+        }
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+        
+    }
+}
