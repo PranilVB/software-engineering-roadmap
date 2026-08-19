80066c8a0ba78f0c7442ec83121ec48a2e395063
pranilvb <pranilvb@gmail.com>
2026-08-19 14:59:13 +0530

    Leetcode 148 sorted Linked List

---
+++ DSA/LinkedList/SortedLinkdList148.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode sortList(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode middle = middleNode(head);
+
+        ListNode temp = head;
+
+        while (temp.next != middle) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+
+        ListNode list1 = sortList(head);
+        ListNode list2 = sortList(middle);
+
+        return mergeTwoLists(list1, list2);
+    }
+
+    private ListNode middleNode(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode listfinal = new ListNode();
+        ListNode temp = listfinal;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+
+            temp = temp.next;
+        }
+
+        if (list1 != null) {
+            temp.next = list1;
+        } else {
+            temp.next = list2;
+        }
+
+        return listfinal.next;
+    }
+}
