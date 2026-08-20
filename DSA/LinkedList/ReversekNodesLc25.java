b88a8d1e4ee5f455bbbadb26930143ba518610ba
pranilvb <pranilvb@gmail.com>
2026-08-20 14:19:05 +0530

    Leetcode 25 reverse k linked List

---
+++ DSA/LinkedList/ReversekNodesLc25.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k == 1 || head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (true) {
+
+            // Check if k nodes are available
+            ListNode check = current;
+            for (int i = 0; i < k; i++) {
+                if (check == null) {
+                    return head;
+                }
+                check = check.next;
+            }
+
+            ListNode last = prev;
+            ListNode newEnd = current;
+
+            for (int i = 0; i < k; i++) {
+                ListNode next = current.next;
+
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            if (last != null) {
+                last.next = prev;
+            } else {
+                head = prev;
+            }
+
+            newEnd.next = current;
+
+            if (current == null) {
+                break;
+            }
+
+            prev = newEnd;
+        }
+
+        return head;
+    }
+}
