5aefd316e344dd0bd986b386423a43ca45bd4d36
pranilvb <pranilvb@gmail.com>
2026-08-20 21:18:13 +0530

    Dynamic Custom Stack

--- Java/Stack & Queue/CustomStack/MainStack.java
+++ Java/Stack & Queue/CustomStack/MainStack.java
@@ -2,7 +2,7 @@ package CustomStack;
 
 public class MainStack {
     public static void main(String[] args) throws Exception{
-        CustomStack stack=new CustomStack(7);
+        DynamicStack stack=new DynamicStack(7);
         stack.push(12);
         stack.push(11);
         stack.push(13);
@@ -10,8 +10,16 @@ public class MainStack {
         stack.push(15);
         stack.push(16);
         stack.push(17);
+        stack.push(14);
+        stack.push(15);
+        stack.push(16);
+        stack.push(17);
         System.out.println("Peek element is : "+stack.peek());
         System.out.println("Peek element is : "+stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
