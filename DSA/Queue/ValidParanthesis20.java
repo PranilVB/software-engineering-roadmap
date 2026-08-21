9cee953c8d776259f9d9e12cb0aa853c5a3966b5
pranilvb <pranilvb@gmail.com>
2026-08-21 23:01:40 +0530

    LC 20 Valid Parentheses

---
+++ DSA/Queue/ValidParanthesis20.java
@@ -0,0 +1,49 @@
+import java.util.Stack;
+
+public class ValidParanthesis20 {
+
+    class Solution {
+
+        public boolean isValid(String s) {
+            Stack<Character> ch = new Stack<>();
+            for (Character cha : s.toCharArray()) {
+                if (cha == '(' || cha == '[' || cha == '{') {
+                    ch.push(cha);
+                } else {
+                    if (cha == ')') {
+                        if (ch.isEmpty()) {
+                            return false;
+                        }
+                        if (ch.peek() == '(') {
+                            ch.pop();
+                        } else {
+                            return false;
+                        }
+                    }
+                    if (cha == ']') {
+                        if (ch.isEmpty()) {
+                            return false;
+                        }
+                        if (ch.peek() == '[') {
+                            ch.pop();
+                        } else {
+                            return false;
+                        }
+                    }
+                    if (cha == '}') {
+                        if (ch.isEmpty()) {
+                            return false;
+                        }
+                        if (ch.peek() == '{') {
+                            ch.pop();
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return ch.isEmpty();
+        }
+    }
+
+}
