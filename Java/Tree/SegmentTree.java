ade696dfafbf9f990be35c3240c60a9005770862
pranilvb <pranilvb@gmail.com>
2026-08-26 23:05:54 +0530

    Segment tree

---
+++ Java/Tree/SegmentTree.java
@@ -0,0 +1,31 @@
+public class SegmentTree {
+    private static class Node{
+        int data;
+        int start;
+        int end;
+        Node left;
+        Node right;
+        public Node(int start,int end){
+            this.start=start;
+            this.end=end;
+        }
+    }
+    Node root;
+    public SegmentTree(int[] arr){
+        this.root=constructTree(arr,0,arr.length-1);
+    }
+    private Node constructTree(int[] arr,int start,int end){
+        if (start==end) {
+            Node leaf=new Node(start, end);
+            leaf.data=arr[start];
+            return leaf;
+        }
+        Node node = new Node(start, end);
+        int mid=(start+end)/2;
+        node.left= this.constructTree(arr, start, mid);
+        node.left= this.constructTree(arr, mid+1, end);
+        node.data=node.left.data+node.right.data;
+        return node;
+
+    }
+}
