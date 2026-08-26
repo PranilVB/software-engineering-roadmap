978ec677029cb1abf04751e587e88ac1669aec96
pranilvb <pranilvb@gmail.com>
2026-08-26 23:53:59 +0530

    Segment tree querry add

--- Java/Tree/SegmentTree.java
+++ Java/Tree/SegmentTree.java
@@ -23,10 +23,13 @@ public class SegmentTree {
         Node node = new Node(start, end);
         int mid=(start+end)/2;
         node.left= this.constructTree(arr, start, mid);
-        node.left= this.constructTree(arr, mid+1, end);
+        node.right= this.constructTree(arr, mid+1, end);
         node.data=node.left.data+node.right.data;
         return node;
 
+    }
+    public void display(){
+        display(root);
     }
     private void display(Node node){
         String str="";
@@ -48,5 +51,40 @@ public class SegmentTree {
         if (node.right!=null) {
             display(node.right);
         }
+    }
+    public int query(int qsi,int qei){
+        return this.query(this.root,qsi, qei);
+    }
+    private int query(Node node,int qsi,int qei){
+        if (qsi<=node.start && qei>=node.end) {
+            return node.data;
+        }else if (qsi>node.end || qei<node.start) {
+            return 0;
+        }else{
+            return this.query(node.left, qsi, qei)+this.query(node.right, qsi, qei);
+        }
+    }
+    public void update(int index ,int value ){
+        this.root.data=update(this.root, index,value);
+    }
+    private int update(Node node , int index ,int value){
+        if (index>=node.start && index<=node.end) {
+            if (index==node.start && index==node.end) {
+                node.data=value;
+                return node.data;
+            }else{
+                int leftans=update(node.left, index,value);
+                int rightans=update(node.right, index,value);
+                node.data=leftans+rightans;
+                return node.data;
+            }
+        }
+        return node.data;
+    }
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5,6,7,8,9,10};
+        SegmentTree tree=new SegmentTree(arr);
+        // tree.display();
+        System.out.println(tree.query(2, 6));
     }
 }
