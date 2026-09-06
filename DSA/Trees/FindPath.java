370101217fb64181ec08a868a8357f654b36132b
pranilvb <pranilvb@gmail.com>
2026-09-06 23:54:20 +0530

    check path

--- DSA/Trees/FindPath.java
+++ DSA/Trees/FindPath.java
@@ -5,6 +5,9 @@ public class FindPath {
         if (i>=arr.length||node.val!=arr[i]) {
             return false;
         }
+        if (node.left==null&&node.right==null&&i==arr.length-1) {
+            return true;
+        }
         return path(node.left, arr,i++)||(node.right, arr,i++)
     }
 }
