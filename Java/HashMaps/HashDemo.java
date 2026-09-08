fa70eb2118d773f1f8a752f920d1a9a823923cc3
pranilvb <pranilvb@gmail.com>
2026-09-08 21:21:00 +0530

    Hash sets

--- Java/HashMaps/HashDemo.java
+++ Java/HashMaps/HashDemo.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class HashDemo {
     public static void main(String[] args) {
@@ -11,5 +12,12 @@ public class HashDemo {
         map2.put("apple", "newton");
         System.out.println(map.get("tony"));
         System.out.println(map.get("apple"));
+        HashSet<Integer>set=new HashSet<>();
+        set.add(3);
+        set.add(32);
+        set.add(3222);
+        set.add(31);
+        System.out.println(set);
+        System.out.println(set.contains(3));
     }
 }
