1b4537188d762cd6ae96007eafee3b8bfbec444e
pranilvb <pranilvb@gmail.com>
2026-09-15 18:14:06 +0530

    count sort using arrays

--- Java/HashMaps/CountSort.java
+++ Java/HashMaps/CountSort.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CountSort {
     public static  void countSort(int[] arr){
@@ -24,9 +26,28 @@ public class CountSort {
             }
         }
     }
+    public static void countSortHash(int[]arr){
+        if(arr==null || arr.length<=1){
+            return ;
+        }
+        int max=Arrays.stream(arr).max().getAsInt();
+        int min=Arrays.stream(arr).max().getAsInt();
+        Map<Integer,Integer>countMap=new HashMap<>();
+        for (int i : arr) {
+            countMap.put(i, countMap.getOrDefault(i, 0));
+        }
+        int index=0;
+        for (int j = min; j <=max; j++) {
+            int count=countMap.getOrDefault(j, 0);
+            for (int i = 0; i < count; i++) {
+                arr[index]=j;
+                index++;
+            }
+        }
+    }
     public static void main(String[] args) {
         int[] arr={9,5,3,6,8,3,2,3,5,6};
-        countSort(arr);
+        countSortHash(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
